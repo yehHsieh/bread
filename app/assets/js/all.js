ef31861ddd7056fe646797b91aa5e7aa6a3ef7ff
york <york0704@gmail.com>
2022-12-01 21:43:25 +0800

    切版90%

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,5 +1,29 @@
 $(function() {
   console.log('Hello Bootstrap5');
+  
+  // register
+const navTab = document.querySelector(".nav-tabs");
+const registerTab = document.querySelector(".registerTab");
+const loginTab = document.querySelector(".loginTab");
+
+navTab.addEventListener("click", e =>{
+    e.preventDefault();
+    let changeTab = e.target.getAttribute("class");
+    console.log(changeTab)
+    if(changeTab != "nav-link active register text-center" && changeTab != "nav-link active loginBtn text-center"){
+      return
+    }else if(changeTab == "nav-link active register text-center"){
+      registerTab.classList.add("d-block");
+      registerTab.classList.remove("d-none");
+      loginTab.classList.remove("d-block");
+      loginTab.classList.add("d-none");
+    }else if(changeTab == "nav-link active loginBtn text-center"){
+      loginTab.classList.remove("d-none");
+      loginTab.classList.add("d-block");
+      registerTab.classList.remove("d-block");
+      registerTab.classList.add("d-none");
+    }
+})
 });
 
 // swiper
@@ -21,3 +45,4 @@ const datepicker = new Datepicker(elem, {
   language: 'zh-TW',
   buttonClass: 'btn',
 }); 
+
